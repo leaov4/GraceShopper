7f9cb09d3b7b37dab067985c208b03c044a04a3c
hannah <contactuser2220@gmail.com>
2020-11-10 17:19:03 -0500

    feature: Sequelize models & associations completed

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,9 @@
 const User = require('./user')
-
+const Product = require('./product')
+const Cart = require('./cart')
+const Order = require('./order')
+const Order_Product = require('./order_product')
+const Cart_Product = require('./cart_product')
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
@@ -13,6 +17,30 @@ const User = require('./user')
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
+User.belongsTo(Order)
+Order.hasMany(User)
+
+Order.belongsTo(Order_Product)
+Order_Product.hasMany(Order)
+
+Product.belongsTo(Order_Product)
+Order_Product.hasMany(Product)
+
+User.belongsTo(Cart)
+Cart.hasMany(User)
+
+Cart.belongsTo(Cart_Product)
+Cart_Product.hasMany(Cart)
+
+Product.belongsTo(Cart_Product)
+Cart_Product.hasMany(Product)
+
 module.exports = {
-  User
+  User,
+  Product,
+  Cart,
+  Order,
+  Order_Product,
+  Cart_Product,
 }
