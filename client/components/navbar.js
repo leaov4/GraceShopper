ceb1f2ba120939b546c0426e5ba5ed9eaf46d6c6
Adria Orenstein <adria.orenstein@gmail.com>
2020-11-11 15:36:26 -0500

    nav links to shop and home

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -8,10 +8,15 @@ const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
     <h1>BOILERMAKER</h1>
     <nav>
+      {/* {fill in logo img later} */}
+      <Link to="/home">Home</Link>
+      <Link to="/products">Shop</Link>
+      {/* {fill in cart img later} */}
+      {/* <Link to="/cart">Cart</Link> */}
+
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
@@ -20,7 +25,6 @@ const Navbar = ({handleClick, isLoggedIn}) => (
         <div>
           {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
         </div>
       )}
     </nav>
@@ -31,17 +35,17 @@ const Navbar = ({handleClick, isLoggedIn}) => (
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.id,
   }
 }
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     handleClick() {
       dispatch(logout())
-    }
+    },
   }
 }
 
@@ -52,5 +56,5 @@ export default connect(mapState, mapDispatch)(Navbar)
  */
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
+  isLoggedIn: PropTypes.bool.isRequired,
 }
