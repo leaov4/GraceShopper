dc96b115f6d8ee642f0337bb3b9726fce380034c
hannah <contactuser2220@gmail.com>
2020-11-11 17:02:27 -0500

    feature: all-products redux store, fixed original broken test specs

--- server/api/products.js
+++ server/api/products.js
@@ -1,4 +1,4 @@
-const router = require('express').Router
+const router = require('express').Router()
 const {Product} = require('../db/models')
 module.exports = router
 
--- server/db/models/user.spec.js
+++ server/db/models/user.spec.js
@@ -15,8 +15,10 @@ describe('User model', () => {
 
       beforeEach(async () => {
         cody = await User.create({
+          firstName: 'cody',
+          lastName: 'theDog',
           email: 'cody@puppybook.com',
-          password: 'bones'
+          password: 'bones',
         })
       })
 
